5a9e097b2d544eb9378838a4e9fd86af0748b70c
KarimElhaggar <karim.elhaggar@gmail.com>
2025-04-30 03:11:31 +0300

    Movies Model

---
+++ movies/src/main/java/com/example/movies/model/Movie.java
@@ -0,0 +1,82 @@
+package com.example.movies.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String movieName;
+    private String author;
+    private int yearReleased;
+    private double rating;
+    private String genre;
+
+
+    public Movie() {}
+
+    // Constructor for Builder
+    private Movie(Builder builder) {
+        this.id = builder.id;
+        this.movieName = builder.movieName;
+        this.author = builder.author;
+        this.yearReleased = builder.yearReleased;
+        this.rating = builder.rating;
+        this.genre = builder.genre;
+    }
+
+    //   public void setId(int id) { this.id = id; }
+ //   public void setMovieName(String movieName) { this.movieName = movieName; }
+ //   public void setAuthor(String author) { this.author = author; }
+  //  public void setYearReleased(int yearReleased) { this.yearReleased = yearReleased; }
+
+    public static class Builder {
+        private int id;
+        private String movieName;
+        private String author;
+        private int yearReleased;
+        private double rating;
+        private String genre;
+
+        public Builder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder movieName(String movieName) {
+            this.movieName = movieName;
+            return this;
+        }
+
+        public Builder author(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public Builder yearReleased(int yearReleased) {
+            this.yearReleased = yearReleased;
+            return this;
+        }
+
+        public Builder rating(double rating) {
+            this.rating = rating;
+            return this;
+        }
+
+        public Builder genre(String genre) {
+            this.genre = genre;
+            return this;
+        }
+
+        public Movie build() {
+            return new Movie(this);
+        }
+    }
+
+}
