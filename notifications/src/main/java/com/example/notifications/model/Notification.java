a821b13e84302bf02503688bbeed924c813ae3a9
ZiadYasser411 <ziadyasser4111@gmail.com>
2025-05-05 21:32:08 +0300

    Repository done

--- notifications/src/main/java/com/example/notifications/model/Notification.java
+++ notifications/src/main/java/com/example/notifications/model/Notification.java
@@ -1,5 +1,6 @@
 package com.example.notifications.model;
 
+import com.example.notifications.constants.NotificationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
---
+++ notifications/src/main/java/com/example/notifications/repository/NotificationRepository.java
@@ -0,0 +1,20 @@
+package com.example.notifications.repository;
+
+import com.example.notifications.model.Notification;
+import com.example.notifications.constants.NotificationType;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+
+    List<Notification> findByUserId(Long userId);
+
+    List<Notification> findByUserIdAndMarkAsReadFalse(Long userId);
+
+    @Query("{'userId': ?0, 'notificationType': ?1, 'markAsRead': false }")
+    List<Notification> findUnreadByType(Long userId, NotificationType type);
+
+    void deleteByUserId(Long userId);
+}
