5e6489d4ec849bf686c4e16a2470934b37eadc00
maryamhelal <maryam.helaal22@gmail.com>
2025-05-09 16:26:46 +0300

    fix send notifications

---
+++ notifications/src/main/java/com/example/notifications/command/CreateNotificationCommand.java
@@ -0,0 +1,28 @@
+package com.example.notifications.command;
+
+import com.example.notifications.constants.NotificationType;
+import com.example.notifications.model.Notification;
+import com.example.notifications.service.NotificationService;
+
+import java.time.LocalDateTime;
+
+public class CreateNotificationCommand implements NotificationCommand {
+    private final NotificationService notificationService;
+    private Long userId;
+    private NotificationType notificationType;
+
+    public CreateNotificationCommand(NotificationService notificationService, NotificationType notificationType, Long userId) {
+        this.notificationService = notificationService;
+        this.notificationType = notificationType;
+        this.userId = userId;
+    }
+
+    @Override
+    public void execute() {
+        Notification notification = new Notification("Message", notificationType, LocalDateTime.now(), userId);
+        notificationService.saveNotification(notification);
+    }
+
+    @Override
+    public void undo() {}
+}
