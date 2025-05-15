9a34e901ea4a4a62a676a7cde2067a04656cf7f7
mohamedahmed1902@yahoo.com <mohamedahmed1902@yahoo.com>
2025-05-15 21:13:37 +0300

    added users client in reviews and updated user controller

--- reviews/src/main/java/com/example/reviews/clients/UsersClient.java
+++ reviews/src/main/java/com/example/reviews/clients/UsersClient.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-// TODO(elgharieb, 13-05-2025) update
 @FeignClient(name = "user-service", url = "http://localhost:8080/users")
 public interface UsersClient {
     @GetMapping("/followers/{id}")
     List<Long> getUserFollowersById(@PathVariable long id);
+
+    @GetMapping("/userExists/{id}")
+    boolean userExists(@PathVariable long id);
 }
