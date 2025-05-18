747d3e1114fc61b739805c39712480c38d93e074
mohamedahmed1902@yahoo.com <mohamedahmed1902@yahoo.com>
2025-05-18 19:33:20 +0300

    added annotation

--- users/src/main/java/com/example/users/UserApplication.java
+++ users/src/main/java/com/example/users/UserApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCaching
 @EnableFeignClients
+@EnableJpaRepositories(basePackages = "com.example.users.repository")
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
