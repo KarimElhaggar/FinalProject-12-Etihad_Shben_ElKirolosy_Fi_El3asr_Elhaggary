2bcfb90aa9a52cd0a888b29e3b49efd0877638af
hazemmohamed210 <hazemanwar349@gmail.com>
2025-05-18 21:58:11 +0300

    add service discovery client and cloud load balancer

--- reviews/src/main/java/com/example/reviews/ReviewApplication.java
+++ reviews/src/main/java/com/example/reviews/ReviewApplication.java
@@ -3,12 +3,14 @@ package com.example.reviews;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableCaching
 @EnableFeignClients
+@EnableDiscoveryClient
 @EnableMongoRepositories(basePackages = "com.example.reviews.repository")
 public class ReviewApplication {
     public static void main(String[] args) {
